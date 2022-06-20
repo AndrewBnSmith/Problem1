429fda7c5b412033aca4f5ca81e82c4ca672aefd
AndrewBnSmith <absmith1058@outlook.com>
2022-06-20 17:25:53 -0400

    completed print statements

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,9 @@ public class Main {
         while (n < 1) {
             System.out.print("Enter a positive number for array size: ");
             n = scan.nextInt();
-
-
         }
 
         int[] array = new int[n];
-
 
         int indexCounter = 0;
         for (int i = 0; i <= array.length - 1; i++) {
@@ -30,28 +27,42 @@ public class Main {
         int min = array[0];
         int max = array[0];
         double total = 0;
-        for(int i = 0; i <= array.length-1; i++){
+        for (int i = 0; i <= array.length - 1; i++) {
 
             total += array[i];
-            if (array[i] > max){
+            if (array[i] > max) {
                 max = array[i];
-            }
-            else if (array[i] < min){
+            } else if (array[i] < min) {
                 min = array[i];
             }
 
-
         }
+
+
 
         System.out.println("Maximum value = " + max);
         System.out.println("Minimum value = " + min);
         System.out.println("Average: " + total / n);
         System.out.println();
         System.out.println("Elements greater than average: ");
+        for(int i = 0; i <= array.length-1; i++){
+            if (array[i] > total/n){
+               System.out.println(array[i]);
+            }
+
+        }
         System.out.println("Elements greater than 2: ");
+        for(int i = 0; i <= array.length-1; i++){
+            if (array[i] > 2){
+                System.out.println(array[i]);
+            }
+
+        }
         System.out.println("Elements in reverse order: ");
+        for(int i = array.length-1 ; i >= 0; i--){
+            System.out.println(array[i]);
 
-
+        }
 
     }
 }
